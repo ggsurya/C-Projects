e9899cc466e9df65e3a9376e94f8a21915b7aaec
GG Surya <ggsuryaff@gmail.com>
2025-08-07 18:01:30 +0530

    Update 03_Temperature_Converter.c

--- 03_Temperature_Converter.c
+++ 03_Temperature_Converter.c
@@ -3,41 +3,53 @@ int main()
 {
     int choice;
     double temp,converted;
-    printf("Temperature Converter\n");
+    printf("====================================\n");
+    printf("        Temperature Converter\n");
+    printf("====================================\n");
     printf("1. Celsius to Fahrenheit\n");
     printf("2. Fahrenheit to Celsius\n");
     printf("3. Celsius to Kelvin\n");
     printf("4. Kelvin to Celsius\n");
+    printf("------------------------------------\n");
     printf("Enter your choice (1-4): ");
     scanf("%d",&choice);
+    printf("\n");
     switch(choice)
     {
         case 1:
-            printf("Enter temperature in Celsius: ");
+            printf(">>> Enter temperature in Celsius: ");
             scanf("%lf",&temp);
             converted=(temp*9/5)+32;
-            printf("%.2lf *C = %.2lf *F\n",temp,converted);
+            printf("--- Result: %.2lf *C = %.2lf *F\n",temp,converted);
             break;
         case 2:
-            printf("Enter temperature in Fahrenheit: ");
+            printf(">>> Enter temperature in Fahrenheit: ");
             scanf("%lf",&temp);
             converted=(temp-32)*5/9;
-            printf("%.2lf *F = %.2lf *C\n",temp,converted);
+            printf("--- Result: %.2lf *F = %.2lf *C\n",temp,converted);
             break;
         case 3:
-            printf("Enter temperature in Celsius: ");
+            printf(">>> Enter temperature in Celsius: ");
             scanf("%lf",&temp);
             converted=temp+273.15;
-            printf("%.2lf *C = %.2lf K\n",temp,converted);
+            printf("--- Result: %.2lf *C = %.2lf K\n",temp,converted);
             break;
         case 4:
-            printf("Enter temperature in Kelvin: ");
+            printf(">>> Enter temperature in Kelvin: ");
             scanf("%lf",&temp);
-            converted=temp-273.15;
-            printf("%.2lf K = %.2lf *C\n",temp,converted);
+            if(temp<0)
+            {
+                printf("!!! Invalid Kelvin value. Must be >= 0\n");
+            }
+            else
+            {
+                converted=temp-273.15;
+                printf("--- Result: %.2lf K = %.2lf *C\n",temp,converted);
+            }
             break;
         default:
-            printf("Invalid choice!\n");
+            printf("Invalid choice..! Please enter a number between 1 and 4\n");
     }
+    printf("====================================\n");
     return 0;
 }
