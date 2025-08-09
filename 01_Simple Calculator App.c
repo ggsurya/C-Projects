a5f18680646ed60ae2eb6d4859dec6ecce707600
GG Surya <ggsuryaff@gmail.com>
2025-08-09 18:48:59 +0530

    Update 01_Simple Calculator App.c

--- 01_Simple Calculator App.c
+++ 01_Simple Calculator App.c
@@ -1,38 +1,52 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    double num1,num2;
+    double num1, num2;
     char op;
+
     printf("====================================\n");
     printf("         Simple Calculator\n");
     printf("====================================\n");
+
     printf("Enter the 1st number     : ");
-    scanf("%lf",&num1);
+    scanf("%lf", &num1);
+
     printf("Enter the 2nd number     : ");
-    scanf("%lf",&num2);
+    scanf("%lf", &num2);
+
     printf("Enter the operator (+ - * /): ");
-    scanf(" %c",&op);
+    scanf(" %c", &op);
+
     printf("\n------------- Result -------------\n");
-    switch(op)
+
+    switch (op)
     {
         case '+':
-            printf("Addition       : %.2lf + %.2lf = %.2lf\n",num1,num2,num1+num2);
+            printf("Addition       : %.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
             break;
+
         case '-':
-            printf("Subtraction    : %.2lf - %.2lf = %.2lf\n",num1,num2,num1-num2);
+            printf("Subraction    : %.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
             break;
+
         case '*':
-            printf("Multiplication : %.2lf * %.2lf = %.2lf\n",num1,num2,num1*num2);
+            printf("Multiplication : %.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
             break;
+
         case '/':
-            if(num2==0)
+            if (num2 == 0)
                 printf("Error          : Division by zero is not allowed.\n");
             else
-                printf("Division       : %.2lf / %.2lf = %.2lf\n",num1,num2,num1/num2);
+                printf("Division       : %.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
             break;
+
         default:
             printf("Error          : Invalid operator entered.\n");
+            break;
     }
+
     printf("====================================\n");
+
     return 0;
 }
